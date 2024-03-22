a5a7530620237b6cc290726f1d0083ebd69d20e6
Eric Ford <ericford@mac.com>
2024-03-22 15:59:38 -0400

    Removed a lot of unneccesary files. Changed title in header.php.

--- _infrastructure/local-env/local-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec /all-entrypoints.sh "$@"
--- header.php
+++ header.php
@@ -2,7 +2,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Project Inclusive — <?php echo $submit_text; ?></title>
+    <title>Disability as Diversity</title>
     <meta name="description" content="MinnoJS">
     <meta name="viewport" content="width=device-width">
     <meta name="viewport" content="user-scalable=no, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1" />
@@ -19,4 +19,4 @@
           <div class="panel panel-info">
             <div class="panel-body">
               <img src="./study/media/P-I_logo.png" class="p-i-logo" />
-              <h1>Clinical Decision-Making in the Prenatal Setting</h1>
+              <h1>Headline Here</h1>
