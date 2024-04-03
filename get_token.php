788fd1e7863932e1c684ebe32b23529b0d0ae410
Eric Ford <ericford@mac.com>
2024-04-03 12:12:26 -0400

    Fixed getting get_token and added validation code. Removed .env (why was that in there?).

--- get_token.php
+++ get_token.php
@@ -2,20 +2,21 @@
   $env_file_path = realpath(__DIR__."/.env");
   //Check .env file exists and is readable.
   if (!is_file($env_file_path) || !is_readable($env_file_path)) {
-    http_response_code(500);
-    echo 'Token not available. Please contact the server administrator with this message.';
-    exit();
+      http_response_code(500);
+      header('Location: 500.html');
   }
   $API_TOKEN = '';
+  $VALID_CODE = '';
   // Open the .en file using the reading mode
   $fopen = fopen($env_file_path, 'r');
+  $env_arr = array();
   if ($fopen) {
     //Loop the lines of the file
     while (($line = fgets($fopen)) !== false) {
         // Check if line is a comment
         $env_ex = preg_split('/(\s?)\=(\s?)/', $line);
-        $env_value = isset($env_ex[1]) ? trim($env_ex[1]) : "";
-        $API_TOKEN = $env_value;
+        $valid = $env_ex[0];
+        $$valid = isset($env_ex[1]) ? trim($env_ex[1]) : "";
     }
     // Close the file
     fclose($fopen);
