8a5f34cdf84f8219fe03370e5b8405a4404140e9
Eric Ford <ericford@mac.com>
2024-04-03 01:07:26 -0400

    Still working on error messages.

--- index.php
+++ index.php
@@ -37,7 +37,10 @@
   require('get_token.php');
   if (!isset($_POST['variable'])) {
     http_response_code(500);
-    echo '500 Internal Server Error. Please contact the administrator.';
+    echo json_encode([
+      'success' => false ,
+      'message' => '500 Forbidden',
+    ]);
     exit();
   } elsif (isset($_POST['project_id'])
         && $_POST['project_id'] == '2601'
